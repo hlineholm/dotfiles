fe95684e89e3b06df47f55850a17fa7a1cdfa1ad
Henrik Lineholm <henrik.lineholm@flir.se>
2019-08-31 14:27:47 +0200

    Fix syntax error for sed

--- bootstrap_flir.sh
+++ bootstrap_flir.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 bash bootstrap.sh
-sed -i -e 's/henrik.lineholm@icloud.com/henrik.lineholm@flir.se/g' ~/.gitconfig
+sed -i '' 's/henrik.lineholm@icloud.com/henrik.lineholm@flir.se/g' ~/.gitconfig
